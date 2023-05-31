fe5456cfe2fbd7ab2efdfc34473a4dc5535c6756
Angey22 <77977289+Angey22@users.noreply.github.com>
2023-05-31 08:22:19 +0000

    Deploying to gh-pages from @ Angey22/goit-js-hw-10@dd14d8ef0fa1e6da924c17522aab6a642794c7fc 🚀

--- index.8db0e8f4.js
+++
@@ -1,2 +0,0 @@
-const e={breed:document.querySelector(".breed-select"),loader:document.querySelector(".loader"),error:document.querySelector(".error"),catInfo:document.querySelector(".cat-info")};e.breed.classList.add("is-hidden"),e.error.classList.add("is-hidden");
-//# sourceMappingURL=index.8db0e8f4.js.map
